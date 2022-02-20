45943a0c95ad5af86010144899c83fc0b0e426de
joshmclynn <mclynn_josh@hotmail.co.uk>
2022-02-20 15:25:33 +0000

    adding html template files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FIND_A_GARDENER.wsgi
--- finder/views.py
+++ finder/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import 
 
 # Create your views here.
+
+class Index():
+    template_name = "index.html"
+    
