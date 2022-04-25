5effcabc1e45e4974f7ce02358054b233edde9d9
joshmclynn <mclynn_josh@hotmail.co.uk>
2022-04-25 17:32:08 +0100

    update on matches() in views
    search was not returning filter based on full logged in users needs, created for loop to show all users where key words in 'needs' were present.

--- finder/views.py
+++ finder/views.py
@@ -24,9 +24,6 @@ def matches(request):
     d = request.user.needs
     ## convert msfList to string for .filter()
     
-    d1 = ''
-    for i in d:
-        d1 = d1+','+i
     
     ##filter to match users to other users(excluding themselves)
     if request.user.is_authenticated:
@@ -35,13 +32,16 @@ def matches(request):
         if (b == 'business'):
             ##returns 'business' users results based on inputs given at sign up
             all_users = user.objects.filter(user_type__icontains='customer')
-            correctusers = all_users.objects.filter(needs__icontains=d1)                           
+            for i in d:
+                correctusers = all_users.filter(needs__icontains=i)                         
             local_users =  correctusers.objects.filter(location__icontains=c)                         
         else:
             ##returns 'customer' user results based on inputs given at sign up
             all_users = user.objects.filter(user_type__icontains='business')
-            
-            correctusers = all_users.filter(needs__icontains=d1)
+            print(all_users)
+            for i in d:
+                correctusers = all_users.filter(needs__icontains=i)
+            print(correctusers)
                                   
             local_users =  correctusers.filter(location__icontains=c) 
             
